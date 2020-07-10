b67c7c8581fd5b3e12e9c635ae1221ff020f95c1
T Lom <tiffanyplombardi@gmail.com>
2020-07-10 16:21:31 -0400

    CSS element

--- script.js
+++ script.js
@@ -1,67 +1,9 @@
-// # 05 Third-Party APIs: Work Day Scheduler
+// HOMEWORK 05 Third-Party APIs: Work Day Scheduler
 
-// Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-// You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
-// ## User Story
-
-// ```
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
-// ```
-
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// ----------- text block created in block
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock atrribute same index as block
-//------------store local
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
-
-// The following animation demonstrates the application functionality:
-
-// ![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
-
-// ## Review
-
-// You are required to submit the following for review:
-
-// * The URL of the deployed application.
-
-// * The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-// - - -
-// © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
-
-// as soon as i open my page get document on ready
-
-// Create time blocks with sub divs
-// use jQuery to add event listener, blocks
-//bunch of rows and 3 colums
-// col 1. time with text moved to the side and borders added, attribute added to target
-// col 2. text box, which needs attribute to help differentiate it
-// col 3. save box, which will locally save whats in the box, needs atrributes aswell
-
-// ----------------------------------------------------------------
-// ------------------------------------------------------------------
 // DEPENDENCIES =========================================================
 var currentDay = document.querySelector("#currentDay");
 
-//TODO: as soon as I open my page get document on ready - this is the beginning! Woo Hoo
+//As soon as I open my page get document on ready - this is the beginning! Woo Hoo
 $(document).ready(function () {
   console.log("ready!");
 });
